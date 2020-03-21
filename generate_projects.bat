ae9ee5baa2e2abefca7f7bf3f2d314064dc636f7
Ivo <i.m.stoilov@gmail.com>
2020-03-21 14:19:08 +0200

    [Tools] Moved premake.exe
    Moved Premake5.exe from extern to tools

--- generate_projects.bat
+++ generate_projects.bat
@@ -1,5 +1,5 @@
 @echo off
-call extern\Premake5\premake5.exe --file=project\_premake_scripts\w_premake_Homebrew.lua vs2019
-call extern\Premake5\premake5.exe --file=project\_premake_scripts\w_premake_SystemSandbox.lua vs2019
-call extern\Premake5\premake5.exe --file=project\_premake_scripts\w_premake_GraphicsSandbox.lua vs2019
+call tools\premake5\premake5.exe --file=project\_premake_scripts\w_premake_Homebrew.lua vs2019
+call tools\premake5\premake5.exe --file=project\_premake_scripts\w_premake_SystemSandbox.lua vs2019
+call tools\premake5\premake5.exe --file=project\_premake_scripts\w_premake_GraphicsSandbox.lua vs2019
 PAUSE
